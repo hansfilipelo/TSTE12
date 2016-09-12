1418cafc702dcf3479af0aab7adb7d2bf0560546
hansfilipelo <hansfilipelo@gmail.com>
2016-09-12 17:32:20 +0200

    Simulated Part1

--- Part1/INL1_KB.vhdl
+++ Part1/INL1_KB.vhdl
@@ -3,7 +3,7 @@
 entity INL1_KB is
     port (
             A : in BIT_VECTOR(3 downto 0);
-            B : in BIT_VECTOR(3 downto 0);
+            B : in BIT;
             Y : out BIT_VECTOR(3 downto 0)
          );
 end INL1_KB;
@@ -14,7 +14,7 @@ begin
         VARIABLE tmp : BIT_VECTOR(3 downto 0);
     begin
         for I in 0 to 3 loop
-            IF A(I) /= B(I) THEN
+            IF A(I) /= B THEN
                 tmp(I) := '1';
             ELSE
                 tmp(I) := '0';
